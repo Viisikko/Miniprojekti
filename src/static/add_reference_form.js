1e5721358b6f219a4373d26e9841ba0aae9c96d2
test <e114@git.pilvi>
2025-11-27 13:38:57 +0200

    added auto increment to index generation logic

--- src/static/add_reference_form.js
+++ src/static/add_reference_form.js
@@ -2,11 +2,36 @@ function generate_index() {
     const year = document.querySelector('[name="year"]').value;
     const authors = document.querySelector('[name="author"]').value.split(",");
     const generated_index = authors.join("").replaceAll(" ", "") + year;
-
-    document.querySelector('[name="index"]').value = generated_index;
+    return generated_index
 }
 
+async function generate_index_request() {
+    let give_up_counter = 0;
+    const result = generate_index();
 
-function handle_reftype(){
+    while (give_up_counter < 10) {
+        let index_name = result;
+        if (give_up_counter != 0){
+            index_name = result + "_" + give_up_counter
+        }
+        const res = await fetch("/index/" + index_name);
+        
+        if (!res.ok){
+            document.querySelector('[name="index"]').value = index_name;
+            break;
+        }
+
+        const result_json = await res.json()
+
+        if (result_json["status"] == false){
+            document.querySelector('[name="index"]').value = index_name;
+            break
+        }
+
+        give_up_counter++;
+    }
+}
+
+function handle_reftype() {
     document.querySelector("form.add_reference").setAttribute("rtype", document.querySelector("[name='type']").value)
 }
