7676daec9bcaf125cea5fa1611a2584ffaa14710
AnteroVehkaoja <apsoluutti@gmail.com>
2025-12-04 07:09:55 +0200

    fix tests

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,5 +1,5 @@
 # Listening only on loopback to avoid accidental direct exposure of the application server without a reverse proxy
-bind = '127.0.0.1:5001' 
+bind = '127.0.0.1:5001'
 
 # Just log to stdout for now
 errorlog = '-'
